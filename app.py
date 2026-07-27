81d8f860bdd39247090727cae54ccfe521f344e7
iftekhar768 <iftekharobito10@gmail.com>
2026-07-28 04:30:12 +0600

    devices databases added

--- app.py
+++ app.py
@@ -1,18 +1,47 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from config import Config
-from models.device import db
+from models.device import db, Device
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
 
+
 @app.route("/")
 def home():
-    return "<h1>NetVision is Running!</h1>"
+    return render_template("dashboard.html")
+
+
+@app.route("/devices")
+def devices():
+    all_devices = Device.query.all()
+    return render_template(
+        "devices.html",
+        devices=all_devices
+    )
+
+@app.route("/add-device", methods=["GET", "POST"])
+def add_device():
+
+    if request.method == "POST":
+
+        device = Device(
+            name=request.form["name"],
+            ip_address=request.form["ip_address"],
+            location=request.form["location"]
+        )
+
+        db.session.add(device)
+        db.session.commit()
+
+        return redirect("/devices")
+
+    return render_template("add_device.html")
 
 with app.app_context():
     db.create_all()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
